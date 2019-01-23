476602e812e473d225ceea70e2248b314875c180
Werner Barnard <wbarnard81@gmail.com>
2019-01-23 15:21:28 +0200

    Added frontent

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts); //first arg is url path and 2nd is route to use
 
+//Handle Production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static(__dirname + '/public'));
+    //Handle SPA
+    app.get(/.*/, (req, res) => 
+        res.sendFile(__dirname + '/public/index.html')
+    );
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
